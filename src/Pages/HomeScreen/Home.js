553cca3acd7c9687e028820cd2622fc933dd317d
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-03-21 09:07:50 +0530

    navigation added to the system

--- src/Pages/HomeScreen/Home.js
+++ src/Pages/HomeScreen/Home.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React from 'react'
+import { View, Text, Button, ScrollView } from 'react-native';
+
+const array = [1,2,3,4,5,6,7,8,9,10];
+
 export default function Home() {
+  console.log('in to Home component')
   return (
     <View>
-      <Text>Home</Text>
+      <ScrollView>
+        {
+          [...array,...array,...array,...array,...array,...array].map((item,index)=>{
+            return <Text key={index}>My Home this containers Text</Text>
+          })
+        }
+        <Button title="go to show notes" color="blue"></Button>
+      </ScrollView>
     </View>
-  );
+  )
 }
