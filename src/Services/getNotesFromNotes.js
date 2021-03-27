8b99b2bb6e0bbc01411dd0f71a5a148ce5d5a9be
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-03-27 16:07:27 +0530

    read notes error fixes

--- src/Services/getNotesFromNotes.js
+++ src/Services/getNotesFromNotes.js
@@ -3,17 +3,22 @@ import RNFS from 'react-native-fs'
 import { EXTERNAL_FILE_STORAGE_PATH, NOTES_FOLDER } from '../Constant'
 
 export default async function getNotesFromNotes() {
-  let files = await RNFS.readDir(
-    EXTERNAL_FILE_STORAGE_PATH + '/' + NOTES_FOLDER,
-  )
   let data = []
-  for (const file of files) {
-    if (file.isFile()) {
-      let fileData = await RNFS.readFile(file.path)
-      let fileObject = JSON.parse(fileData);
-      fileObject.path = file.path;
-      data.push(fileObject)
+  try {
+    let files = await RNFS.readDir(
+      EXTERNAL_FILE_STORAGE_PATH + '/' + NOTES_FOLDER,
+    )
+    for (const file of files) {
+      if (file.isFile()) {
+        let fileData = await RNFS.readFile(file.path)
+        let fileObject = JSON.parse(fileData)
+        fileObject.path = file.path
+        data.push(fileObject)
+      }
     }
+    return data
+  } catch (err) {
+    console.log('err while fetching :', err)
+    return data
   }
-  return data
 }
