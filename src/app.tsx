a1f5f678ab29388c2f422356593b3f0d03f43cce
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-03-28 10:00:53 +0530

    added typescript

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import NavigationComponent from './components/NavigationComponent'
+
+export default function App() {
+  return <NavigationComponent />
+}
