e3657b11cba9ccb41d30a7b7fc938287e21dfae6
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-03-28 10:01:51 +0530

    changed app.tsx file

--- App.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import NavigationComponent from './src/components/NavigationComponent'
-
-export default function App() {
-  return <NavigationComponent />
-}
